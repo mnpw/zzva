7a6c306faf7c458867dd029f937a6720ad87ab45
Mrinal Paliwal <mrinal16164@iiitd.ac.in>
2022-07-01 11:50:52 +0530

    Expose all endpoints

---
+++ zzva-web/src/main.rs
@@ -0,0 +1,24 @@
+use actix_web::{web, App, HttpServer};
+use zzva_web::{
+    routes::{health_check, play, start, start_default, view},
+    AppState,
+};
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let app = web::Data::new(AppState::default());
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(app.clone())
+            .route("/health_check", web::get().to(health_check))
+            .route("/start", web::post().to(start))
+            .route("/start_default", web::post().to(start_default))
+            .route("/play", web::post().to(play))
+            .route("/view", web::get().to(view))
+            .route("/view_raw", web::get().to(view))
+    })
+    .bind("0:8080")?
+    .run()
+    .await
+}
