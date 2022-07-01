0769673b4b0d0b8ec9154ee09309f2b47a92ecd1
Mrinal Paliwal <mrinal16164@iiitd.ac.in>
2022-07-01 11:37:36 +0530

    Remove unused deps

--- zzva-web/src/routes/health_check.rs
+++ zzva-web/src/routes/health_check.rs
@@ -1,5 +1,5 @@
-use actix_web::{HttpRequest, HttpResponse};
+use actix_web::HttpResponse;
 
-pub async fn health_check(_req: HttpRequest) -> HttpResponse {
+pub async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
