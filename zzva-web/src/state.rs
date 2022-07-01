0c44f7912209b1859c19578b8be75f0f83f1aa75
Mrinal Paliwal <mrinal16164@iiitd.ac.in>
2022-07-01 12:23:02 +0530

    Add reset game method

--- zzva-web/src/state.rs
+++ zzva-web/src/state.rs
@@ -15,6 +15,12 @@ impl AppState {
     pub fn get_state(&self) -> std::sync::MutexGuard<'_, Option<State>> {
         self.state.lock().unwrap()
     }
+
+    pub fn reset_game(&self) {
+        // retain previous state and reset game
+
+        *self.get_game() = None;
+    }
 }
 
 impl Default for AppState {
